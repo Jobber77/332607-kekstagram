2ee33962f7ff8760591704fe565f34e354aeabb1
Jobber77 <joalexey@gmail.com>
2018-04-26 14:25:30 +0300

    fixes after mentor feedback

--- js/backend.js
+++ js/backend.js
@@ -2,48 +2,36 @@
 'use strict';
 
 (function () {
-  var getData = function (link, onLoad, onError) {
+  var TIMEOUT_VALUE = 10000;
+  var getXHRObject = function (onLoadMethod, onErrorMethod, timeout) {
     var xhr = new XMLHttpRequest();
-
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
       if (xhr.status === 200) {
-        onLoad(xhr.response);
+        onLoadMethod(xhr.response);
       } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        onErrorMethod('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
       }
     });
     xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
+      onErrorMethod('Произошла ошибка соединения');
     });
     xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout / 1000 + 'с. Попробуйте еще раз');
+      onErrorMethod('Запрос не успел выполниться за ' + xhr.timeout / 1000 + 'с. Попробуйте еще раз');
     });
-    xhr.timeout = 10000; // 10 sec
+    xhr.timeout = timeout;
+    return xhr;
+  };
+  var getData = function (link, onLoad, onError) {
+    var xhr = getXHRObject(onLoad, onError, TIMEOUT_VALUE);
     xhr.open('GET', link);
     xhr.send();
   };
   var postData = function (data, link, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.responseType = 'json';
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onLoad(xhr.response);
-      } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout / 1000 + 'с. Попробуйте еще раз');
-    });
-    xhr.timeout = 10000; // 10 sec
+    var xhr = getXHRObject(onLoad, onError, TIMEOUT_VALUE);
     xhr.open('POST', link);
     xhr.setRequestHeader('content-type', 'multipart/form-data');
-    xhr.send();
+    xhr.send(data);
   };
 
   window.backend = {
