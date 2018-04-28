151bb9785d08fc9abf35aa80139ad302daa92da2
Jobber77 <joalexey@gmail.com>
2018-04-28 14:20:12 +0300

    fixes after feedback

--- js/backend.js
+++ js/backend.js
@@ -3,7 +3,7 @@
 
 (function () {
   var TIMEOUT_VALUE = 10000;
-  var getXHRObject = function (onLoadMethod, onErrorMethod, timeout) {
+  var getXHRObject = function (onLoadMethod, onErrorMethod) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     xhr.addEventListener('load', function () {
@@ -17,20 +17,19 @@
       onErrorMethod('Произошла ошибка соединения');
     });
     xhr.addEventListener('timeout', function () {
-      onErrorMethod('Запрос не успел выполниться за ' + xhr.timeout / 1000 + 'с. Попробуйте еще раз');
+      onErrorMethod('Запрос не успел выполниться за ' + xhr.timeout + 'мс. Попробуйте еще раз');
     });
-    xhr.timeout = timeout;
+    xhr.timeout = TIMEOUT_VALUE;
     return xhr;
   };
   var getData = function (link, onLoad, onError) {
-    var xhr = getXHRObject(onLoad, onError, TIMEOUT_VALUE);
+    var xhr = getXHRObject(onLoad, onError);
     xhr.open('GET', link);
     xhr.send();
   };
   var postData = function (data, link, onLoad, onError) {
-    var xhr = getXHRObject(onLoad, onError, TIMEOUT_VALUE);
+    var xhr = getXHRObject(onLoad, onError);
     xhr.open('POST', link);
-    xhr.setRequestHeader('content-type', 'multipart/form-data');
     xhr.send(data);
   };
 
